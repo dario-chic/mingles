3b14d45b72c9e12faa49fec91d33a111f653a4e1
Dario Chic <dariochic@gmail.com>
2022-10-03 15:25:29 -0400

    feat SSR

---
+++ components/Team.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Team = () => {
+  return (
+    <section
+      className="questions"
+      id="team"
+      data-aos="fade-up"
+      data-aos-duration="800"
+      data-aos-delay="500"
+      data-aos-once="true"
+    >
+      Team
+    </section>
+  );
+};
+
+export default Team;
